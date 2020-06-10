4f6a338447df31136d09168c443208b49cc53b48
Eric Hsu <eric.hsu@kkday.com>
2020-06-10 17:58:00 +0800

    chore: project init

---
+++ GitHubFolks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFolks
+//
+//  Created by Eric Hsu on 2020/6/10.
+//  Copyright © 2020 Edgenta UEMS Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
