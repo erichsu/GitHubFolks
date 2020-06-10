3acdf48662ba412aee6862e9716abf333b303fcd
Eric Hsu <eric.hsu@kkday.com>
2020-06-10 19:20:05 +0800

    feat: add users list data model

---
+++ GitHubFolks/Services/GitHubAPI/Responses/GitHubUserResp.swift
@@ -0,0 +1,50 @@
+//
+//  GitHubUserResp.swift
+//  GitHubFolks
+//
+//  Created by Eric Hsu on 2020/6/10.
+//  Copyright © 2020 Anonymous Ltd. All rights reserved.
+//
+
+import Foundation
+typealias GitHubUsersResp = [GitHubUser]
+struct GitHubUser: Codable {
+    let login: String
+    let id: Int
+    let nodeId: String
+    let avatarUrl: URL
+    let gravatarId: String
+    let url: URL
+    let htmlUrl: URL
+    let followersUrl: URL
+    let followingUrl: String
+    let gistsUrl: String
+    let starredUrl: String
+    let subscriptionsUrl: URL
+    let organizationsUrl: URL
+    let reposUrl: URL
+    let eventsUrl: String
+    let receivedEventsUrl: URL
+    let type: String
+    let siteAdmin: Bool
+    private enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case nodeId = "node_id"
+        case avatarUrl = "avatar_url"
+        case gravatarId = "gravatar_id"
+        case url
+        case htmlUrl = "html_url"
+        case followersUrl = "followers_url"
+        case followingUrl = "following_url"
+        case gistsUrl = "gists_url"
+        case starredUrl = "starred_url"
+        case subscriptionsUrl = "subscriptions_url"
+        case organizationsUrl = "organizations_url"
+        case reposUrl = "repos_url"
+        case eventsUrl = "events_url"
+        case receivedEventsUrl = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+    }
+}
